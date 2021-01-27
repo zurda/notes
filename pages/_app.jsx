c409751589e89fa8050c00a3878441f8ae3d7d5d
zurda <m.weizman@gmail.com>
2021-01-27 16:35:34 +0000

    Apply font using theme-ui theme

---
+++ pages/_app.jsx
@@ -0,0 +1,10 @@
+import { ThemeProvider } from 'theme-ui'
+import theme from '../theme'
+
+export default function App({ Component, pageProps }) {
+  return (
+   <ThemeProvider theme={theme}>
+      <Component {...pageProps} />
+    </ThemeProvider>
+  )
+}
---
+++ theme.js
@@ -0,0 +1,10 @@
+import { roboto } from '@theme-ui/presets'
+
+const theme = {
+  ...roboto,
+  styles: {
+    ...roboto.styles
+  }
+}
+
+export default theme
